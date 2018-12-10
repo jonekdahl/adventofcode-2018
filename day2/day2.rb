aea62748668256bddf822d41c6f40343f39ec7b3
Jon Ekdahl <jon.ekdahl@gmail.com>
2018-12-10 19:49:12 +0100

    Day 2, part two

--- day2/day2.rb
+++ day2/day2.rb
@@ -27,8 +27,21 @@ def checksum(box_ids)
   twice_count * thrice_count
 end
 
-
 box_ids = parse_box_ids
-#box_ids = %w(abcdef bababc abbcde abcccd aabcdd abcdee ababab)
 checksum = checksum(box_ids)
 puts "Checksum: #{checksum}"
+
+
+def differing_letters(b1, b2)
+  b1.each_char.zip(b2.each_char).reduce(0) { |acc, pair| acc += (pair[0] == pair[1] ? 0 : 1) }
+end
+
+def similar_box_ids(box_ids)
+  box_ids.each do |b1|
+    box_ids.each do |b2|
+      return [b1, b2] if differing_letters(b1, b2) == 1
+    end
+  end
+end
+
+puts "Similar ids: #{similar_box_ids(box_ids)}"
